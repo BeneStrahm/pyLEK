4a779f5e3654028fa34591e46050a4c42ca3aaa4
Benedikt Strahm <ac135564@uni-stuttgart.de>
2020-09-16 16:14:20 +0200

    Initial commit: Write string to txt file

---
+++ helpers/txtEditor.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+# Description:  Helper functions for csv file handling
+# Author:       benedikt.strahm@ilek.uni-stuttgart.de
+# Created:      2020-09-16
+# Execution:    Import functions / collections (from helpers.util import func)
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Libraries
+# ------------------------------------------------------------------------------                                                       
+import csv
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+      
+def writeToTxt(fname, txtline, writeMode='w'):
+    """Writing lines to .txt files
+    :param fname: str w/ name of .txt file to write to
+    :param txtline: str w/ text content
+    :param writeMode: str w/ how to open file ('a', 'w')
+    """
+    with open(fname, writeMode) as txt:
+        txt_writer = csv.writer(txt, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+        txt_writer.writerow([txtline])
+
+                    
+
+
