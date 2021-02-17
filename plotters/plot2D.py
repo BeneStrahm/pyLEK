288c50b6b24886dff6894ce42a68e626595dc396
BeneStrahm <benedikt.strahm@ilek.uni-stuttgart.de>
2021-02-17 13:33:40 +0100

    fixed import statements

--- plotters/plot2D.py
+++ plotters/plot2D.py
@@ -2,7 +2,7 @@
 # Description:  Plotting 2-D Lines on one figure
 # Author:       benedikt.strahm@ilek.uni-stuttgart.de
 # Created:      2018-12-06
-# Execution:    Import functions / collections (from helpers import util)
+# Execution:    Import functions / collections (from pyLEK.helpers import util)
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # Libraries
